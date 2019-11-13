e7a5e354a7c16e5170fdb83b2b1e64a185d01f8f
saad alhashimi <saad.alhashimi86@gmail.com>
2019-11-13 15:06:40 +0100

    first  Dashboard layout

--- homeautomation-frontend/App.js
+++ homeautomation-frontend/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Dashboard from "./components/Dashboard";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Malzeit! Un' nu' glozzn of un' orbeiden</Text>
+      <Dashboard />
     </View>
   );
 }
@@ -12,8 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
