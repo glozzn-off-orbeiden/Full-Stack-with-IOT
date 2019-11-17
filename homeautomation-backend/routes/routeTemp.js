ce5dd12b355fd24e962400e7e276546fde452697
andreas <andreass.ziegler@gmail.com>
2019-11-17 13:57:28 +0100

    backend in routes folder controllerTemp.js file .temp changed to .status

--- homeautomation-backend/routes/routeTemp.js
+++ homeautomation-backend/routes/routeTemp.js
@@ -2,6 +2,6 @@ const express = require("express");
 const Router = express.Router();
 const controllerTemp = require("../controller/controllerTemp");
 
-Router.get("/", controllerTemp.temps);
+Router.get("/", controllerTemp.status);
 
 module.exports = Router;
