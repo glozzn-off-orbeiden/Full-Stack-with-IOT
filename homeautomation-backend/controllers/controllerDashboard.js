cc2735dbe9d8e71cb1ab203427469d506c33abb9
lmlovas <lorraine.lovas@gmail.com>
2019-11-19 15:59:57 +0100

    not working at all

--- homeautomation-backend/controllers/controllerDashboard.js
+++ homeautomation-backend/controllers/controllerDashboard.js
@@ -1,54 +1,53 @@
-'use strict'
+"use strict";
 
-const routeDashBoard = require('../routes/routeDashboard');
-const createError = require('http-errors');
-const Status = require('../models/schemaStatus');
-const Temp = require('../models/schemaTemp');
+const routeDashBoard = require("../routes/routeDashboard");
+const createError = require("http-errors");
+const Status = require("../models/schemaStatus");
+const Temp = require("../models/schemaTemp");
 
 // const cursor = db.collection('inventory').find({
 //     'instock.qty': { $lte: 20 }
 //   });
 
 async function fetchstatus(req, res, next) {
+  // console.log(req);
 
-// console.log(req);
+  // const cursor = db.collection('inventory').find({
+  //     instock: { $elemMatch: { qty: 5, warehouse: 'A' } }
+  //   });
+  // const cursor = db.collection('inventory').find({
+  //     status: { $in: ['A', 'D'] }
+  //   });
 
-// const cursor = db.collection('inventory').find({
-//     instock: { $elemMatch: { qty: 5, warehouse: 'A' } }
-//   });
-// const cursor = db.collection('inventory').find({
-//     status: { $in: ['A', 'D'] }
-//   });
+  try {
+    console.log("test");
 
-    try {
-        console.log('test');
+    Status.Light.find(
+      {
+        Status: { $all: "off" }
+      },
+      function(err, response) {
+        if (err) {
+          console.log("i am an error", err);
 
-        Status.Light.findOne ({
-            'Status': {$in:'off'}
+          return next(createError(500, err.message));
+        }
 
-        }, function (err, response) {
+        if (response) {
+          console.log(response);
 
-            if (err) {
-                console.log('i am an error', err);
-
-                return next(createError(500, err.message))
-            }
-
-         if(response){
-             console.log(response);
-
-            res.send(response)
-          }
+          res.send(response);
+        }
         //  else if (Status !== 'on'){
         //      res.send('off')
         //  }
-         })
-    } catch (error) {
-        () => res.send('this is our error mssg', error.message)
-    }
-
+      }
+    );
+  } catch (error) {
+    () => res.send("this is our error mssg", error.message);
+  }
 }
 
 module.exports = {
-    status: fetchstatus
-}
+  status: fetchstatus
+};
